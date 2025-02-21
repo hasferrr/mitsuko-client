81113faebf525a75227fbb8c9545922304920890
hasferrr <79697518+hasferrr@users.noreply.github.com>
2025-02-21 18:26:11 +0700

    chore: remove unused function and clean up semicolon

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -7,58 +7,32 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 /**
- * Debounces a function, ensuring it is only called after a specified delay
- * since the last time it was invoked. This is useful for preventing
- * functions from being called too frequently, such as when handling
- * events like typing, scrolling, or resizing.
- *
- * @param func The function to debounce.
- * @param wait The delay in milliseconds.
- * @returns A debounced version of the function.
- */
-export function debounce<T extends (...args: any[]) => any>(func: T, wait: number): (...args: Parameters<T>) => void {
-  let timeout: ReturnType<typeof setTimeout> | null
-
-  return function executedFunction(...args: Parameters<T>) {
-    const later = () => {
-      timeout = null
-      func(...args)
-    }
-
-    if (timeout !== null) {
-      clearTimeout(timeout)
-    }
-    timeout = setTimeout(later, wait)
-  }
-}
-
-/**
  * Converts a Timestamp object to a string in the format "HH:MM:SS,MS".
  *
  * @param timestamp The Timestamp object to convert.
  * @returns The formatted timestamp string.
  */
 export function timestampToString(timestamp: Timestamp): string {
-  const hours = String(timestamp.h).padStart(2, '0');
-  const minutes = String(timestamp.m).padStart(2, '0');
-  const seconds = String(timestamp.s).padStart(2, '0');
-  const milliseconds = String(timestamp.ms).padStart(3, '0');
+  const hours = String(timestamp.h).padStart(2, '0')
+  const minutes = String(timestamp.m).padStart(2, '0')
+  const seconds = String(timestamp.s).padStart(2, '0')
+  const milliseconds = String(timestamp.ms).padStart(3, '0')
 
-  return `${hours}:${minutes}:${seconds},${milliseconds}`;
+  return `${hours}:${minutes}:${seconds},${milliseconds}`
 }
 
 export function capitalizeWords(inputString: string): string {
-  const words: string[] = inputString.split(' ');
-  const modifiedWords: string[] = [];
+  const words: string[] = inputString.split(' ')
+  const modifiedWords: string[] = []
   for (const word of words) {
     if (word) {
-      const modifiedWord: string = word.charAt(0).toUpperCase() + word.slice(1);
-      modifiedWords.push(modifiedWord);
+      const modifiedWord: string = word.charAt(0).toUpperCase() + word.slice(1)
+      modifiedWords.push(modifiedWord)
     } else {
-      modifiedWords.push('');
+      modifiedWords.push('')
     }
   }
-  return modifiedWords.join(' ');
+  return modifiedWords.join(' ')
 }
 
 export function minMax(value: number, min: number, max: number): number {
