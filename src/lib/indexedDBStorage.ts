20a2939fee517e8ba1f4ecf44a5103149bfc16e0
hasferrr <79697518+hasferrr@users.noreply.github.com>
2025-02-23 18:56:17 +0700

    fix(indexedDBStorage): add window check before accessing IndexedDB methods

--- src/lib/indexedDBStorage.ts
+++ src/lib/indexedDBStorage.ts
@@ -4,8 +4,11 @@ import { set, get, del } from "idb-keyval"
 export const indexedDBStorage: StateStorage = {
   getItem: async (name: string): Promise<string | null> => {
     try {
-      const value = await get(name)
-      return value !== undefined ? value : null;
+      if (typeof window !== "undefined") {
+        const value = await get(name)
+        return value ?? null
+      }
+      return null
     } catch (error) {
       console.error("Error getting item from IndexedDB:", error)
       return null
@@ -13,14 +16,18 @@ export const indexedDBStorage: StateStorage = {
   },
   setItem: async (name: string, value: string): Promise<void> => {
     try {
-      await set(name, value)
+      if (typeof window !== "undefined") {
+        await set(name, value)
+      }
     } catch (error) {
       console.error("Error setting item in IndexedDB:", error)
     }
   },
   removeItem: async (name: string): Promise<void> => {
     try {
-      await del(name)
+      if (typeof window !== "undefined") {
+        await del(name)
+      }
     } catch (error) {
       console.error("Error removing item in IndexedDB:", error)
     }
