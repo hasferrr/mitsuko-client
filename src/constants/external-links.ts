ac48868a393822b29d17619bb2d19708cb93fc33
hasferrr <79697518+hasferrr@users.noreply.github.com>
2025-03-10 17:14:07 +0700

    chore: change github link

--- src/constants/external-links.ts
+++ src/constants/external-links.ts
@@ -1,5 +1,5 @@
 export const DEPLOYMENT_URL = "https://translator.mitsuko.web.id"
 export const NAVBAR_IMG_LINK = "https://i.imgur.com/b9gRjVi.jpeg"
 export const DISCORD_LINK = "https://discord.gg/8PaGWY6FdZ"
-export const GITHUB_LINK = "https://github.com/hasferrr/chizuru-translator"
+export const GITHUB_LINK = "https://github.com/hasferrr"
 export const CHANGE_LOG_LINK = "https://hasferrr.notion.site/Mitsuko-Changelog-1a867529561780a398e9d9f4ed5d73c2"
