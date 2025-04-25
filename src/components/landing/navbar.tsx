d7f9f23470e59b7d0829106659b5775bb3c9a64c
hasferrr <79697518+hasferrr@users.noreply.github.com>
2025-04-25 17:19:02 +0700

    refactor(landing/navbar): refactor solutions dropdown and add 'For Fansubbers' option

--- src/components/landing/navbar.tsx
+++ src/components/landing/navbar.tsx
@@ -42,30 +42,21 @@ export default function Navbar() {
               <div
                 className="absolute left-0 mt-2 w-48 rounded-md bg-white dark:bg-gray-900 p-2 shadow-lg opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all duration-200"
               >
-                <Button
-                  variant="ghost"
-                  className="font-normal w-full text-left block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-800 rounded-md"
-                >
-                  For Content Creators
-                </Button>
-                <Button
-                  variant="ghost"
-                  className="font-normal w-full text-left block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-800 rounded-md"
-                >
-                  For Translators
-                </Button>
-                <Button
-                  variant="ghost"
-                  className="font-normal w-full text-left block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-800 rounded-md"
-                >
-                  For Studios
-                </Button>
-                <Button
-                  variant="ghost"
-                  className="font-normal w-full text-left block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-800 rounded-md"
-                >
-                  For Individuals
-                </Button>
+                {[
+                  "For Content Creators",
+                  "For Translators",
+                  "For Studios",
+                  "For Fansubbers",
+                  "For Individuals",
+                ].map((text, index) => (
+                  <Button
+                    key={`nav-solution-${index}`}
+                    variant="ghost"
+                    className="font-normal w-full text-left block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-800 rounded-md"
+                  >
+                    {text}
+                  </Button>
+                ))}
               </div>
             </div>
             <Link
