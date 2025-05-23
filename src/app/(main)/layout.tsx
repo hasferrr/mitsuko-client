956050bfa92f85bb5284f40ad9a276de8a05c131
hasferrr <79697518+hasferrr@users.noreply.github.com>
2025-05-24 06:37:41 +0700

    chore(layout): remove discount AnnouncementPopUp

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -4,7 +4,7 @@ import { PropsWithChildren } from "react"
 import { AppSidebarWrapper } from "@/components/sidebar/app-sidebar-wrapper"
 import { Metadata } from "next"
 import { META_TITLE } from "@/constants/metadata"
-import AnnouncementPopUp from "@/components/announcement-popup"
+
 export const metadata: Metadata = {
   title: META_TITLE,
 }
@@ -12,7 +12,6 @@ export const metadata: Metadata = {
 export default function Layout({ children }: PropsWithChildren) {
   return (
     <div className="bg-background text-foreground min-h-screen">
-      <AnnouncementPopUp />
       <AppSidebarWrapper header={<Navbar />}>
         <div className="flex flex-col min-h-[calc(100vh-3.6rem)]">
           <div className="md:mx-8 flex flex-grow">
