d6b8e2adedf40f2732c7241908e3e26373a451cd
hasferrr <79697518+hasferrr@users.noreply.github.com>
2025-05-27 18:48:02 +0700

    fix(constants/external-links): update CONTACT_LINK value

--- src/constants/external-links.ts
+++ src/constants/external-links.ts
@@ -3,4 +3,4 @@ export const DEPLOYMENT_URL = url?.endsWith("/") ? url.slice(0, url.length - 1) 
 export const DISCORD_LINK = "https://discord.gg/8PaGWY6FdZ"
 export const GITHUB_LINK = "https://github.com/hasferrr"
 export const FEEDBACK_LINK = "https://mitsuko.canny.io"
-export const CONTACT_LINK = "YldsMGMzVnJiMmhsYkhCQWNISnZkRzl1TG0xbA=="
+export const CONTACT_LINK = "YldsMGMzVnJieTVoY0hCQWNISnZkRzl1TG0xbA=="
