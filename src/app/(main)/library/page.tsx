ba504d44fa1a1b364df55d4b366d1bd98baefefd
hasferrr <79697518+hasferrr@users.noreply.github.com>
2025-07-11 07:19:19 +0700

    feat(library): add canonical URL for library page

--- src/app/(main)/library/page.tsx
+++ src/app/(main)/library/page.tsx
@@ -1,4 +1,12 @@
 import LibraryView from '@/components/library/library-view'
+import { DEPLOYMENT_URL } from '@/constants/external-links'
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  alternates: {
+    canonical: DEPLOYMENT_URL + '/library',
+  },
+}
 
 export default function LibraryPage() {
   return <LibraryView />
