9cce417e71bcd555a6f95043860eeb8d35a7ffba
hasferrr <79697518+hasferrr@users.noreply.github.com>
2025-07-20 04:07:51 +0700

    refactor(transcription): extract main component from transcription page

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,7 +8,6 @@ const eslintConfig = [
   ...compat.config({
     extends: ['next/core-web-vitals', 'next/typescript'],
     rules: {
-      'react-hooks/rules-of-hooks': 'off',
       'react/display-name': 'off',
       'react/no-unescaped-entities': 'off',
       'react-hooks/exhaustive-deps': 'off',
