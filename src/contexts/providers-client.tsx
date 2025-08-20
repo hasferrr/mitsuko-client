cbf3267bb243b7791f1a939e7e4e304802bfb5e5
hasferrr <79697518+hasferrr@users.noreply.github.com>
2025-08-20 12:59:39 +0700

    chore: remove stagewise toolbar and deps

--- src/contexts/providers-client.tsx
+++ src/contexts/providers-client.tsx
@@ -9,8 +9,6 @@ import { useThemeStore } from "@/stores/use-theme-store"
 import { ModelCostProvider } from "@/contexts/model-cost-context"
 import { ModelCreditCost } from "@/types/model-cost"
 import { ClientIdProvider } from "@/contexts/client-id-context"
-import { StagewiseToolbar } from '@stagewise/toolbar-next'
-import ReactPlugin from '@stagewise-plugins/react'
 
 interface ProvidersProps extends PropsWithChildren {
   modelCosts: Map<string, ModelCreditCost>
@@ -59,7 +57,6 @@ export default function ProvidersClient({ children, modelCosts }: ProvidersProps
             <ModelCostProvider value={modelCosts}>
               <ClientIdProvider>
                 {children}
-                <StagewiseToolbar config={{ plugins: [ReactPlugin] }} />
               </ClientIdProvider>
             </ModelCostProvider>
           </UnsavedChangesProvider>
