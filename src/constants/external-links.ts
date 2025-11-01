efd59e4c67f98ae48723b14c913591ce8e4b0231
hasferrr <79697518+hasferrr@users.noreply.github.com>
2025-11-01 07:28:02 +0700

    chore: update discord link

--- src/constants/external-links.ts
+++ src/constants/external-links.ts
@@ -1,5 +1,5 @@
 const url = process.env.NEXT_PUBLIC_DEPLOYMENT_URL || "https://www.mitsuko.app"
 export const DEPLOYMENT_URL = url?.endsWith("/") ? url.slice(0, url.length - 1) : url
-export const DISCORD_LINK = "https://discord.gg/8PaGWY6FdZ"
+export const DISCORD_LINK = "https://discord.mitsuko.app"
 export const GITHUB_LINK = "https://github.com/hasferrr"
 export const CONTACT_LINK = "YldsMGMzVnJieTVoY0hCQWNISnZkRzl1TG0xbA=="
