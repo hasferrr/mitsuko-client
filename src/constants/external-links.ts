43e7dc364e3e9b4c05b812a0020eb451c34a0833
hasferrr <79697518+hasferrr@users.noreply.github.com>
2026-02-20 03:42:41 +0700

    chore: update contact email

--- src/constants/external-links.ts
+++ src/constants/external-links.ts
@@ -2,4 +2,4 @@ const url = process.env.NEXT_PUBLIC_DEPLOYMENT_URL || "https://www.mitsuko.app"
 export const DEPLOYMENT_URL = url?.endsWith("/") ? url.slice(0, url.length - 1) : url
 export const DISCORD_LINK = "https://discord.mitsuko.app"
 export const GITHUB_LINK = "https://github.com/hasferrr"
-export const CONTACT_LINK = "YldsMGMzVnJieTVoY0hCQWNISnZkRzl1TG0xbA=="
+export const CONTACT_LINK = "YzNWd2NHOXlkRUJ0YVhSemRXdHZMbUZ3Y0E9PQ=="
