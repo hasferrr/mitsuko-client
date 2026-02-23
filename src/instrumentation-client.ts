b3d3779d5ecbda2e6b4e371b64fbb5c00e330694
hasferrr <79697518+hasferrr@users.noreply.github.com>
2026-02-23 07:46:11 +0700

    chore(sentry): only send session replays on error and clean up config

--- src/instrumentation-client.ts
+++ src/instrumentation-client.ts
@@ -5,8 +5,6 @@ import { supabase } from "./lib/supabase"
 
 Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
-
-  // Add optional integrations for additional features
   integrations: [
     Sentry.replayIntegration({
       maskAllText: false,
@@ -16,22 +14,10 @@ Sentry.init({
       supabaseClient: supabase,
     }),
   ],
-
-  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
-  // Enable logs to be sent to Sentry
   enableLogs: true,
-
-  // Define how likely Replay events are sampled.
-  // This sets the sample rate to be 10%. You may want this to be 100% while
-  // in development and sample at a lower rate in production
-  replaysSessionSampleRate: 0.1,
-
-  // Define how likely Replay events are sampled when an error occurs.
+  replaysSessionSampleRate: 0,
   replaysOnErrorSampleRate: 1.0,
-
-  // Enable sending user PII (Personally Identifiable Information)
-  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
   sendDefaultPii: true,
 })
 
